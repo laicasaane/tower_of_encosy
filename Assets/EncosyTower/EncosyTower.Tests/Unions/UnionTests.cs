a0b30d0be04e1b17e648e659de270e5d5a89b569
Laicasaane <laicasaane@outlook.com>
2024-11-04 11:48:14 +0700

    Add performance test for big union of 9996 bytes

--- Assets/EncosyTower/EncosyTower.Tests/Unions/UnionTests.cs
+++ Assets/EncosyTower/EncosyTower.Tests/Unions/UnionTests.cs
@@ -1,6 +1,9 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using EncosyTower.Modules.Unions;
 using NUnit.Framework;
 using Unity.PerformanceTesting;
+using UnityEngine.Profiling;
 
 namespace EncosyTower.Tests
 {
@@ -12,9 +15,13 @@ namespace EncosyTower.Tests
             int i = 0;
 
             Measure.Method(() => {
-                var u = new Union(i);
-                Union_Int(u);
-                i++;
+                Profiler.BeginSample("Union");
+                {
+                    var u = new Union(i);
+                    Process(u);
+                    i++;
+                }
+                Profiler.EndSample();
             })
                 .WarmupCount(1)
                 .IterationsPerMeasurement(5000)
@@ -22,9 +29,51 @@ namespace EncosyTower.Tests
                 .Run();
         }
 
-        private static void Union_Int(in Union union)
+        [Test, Performance]
+        public void UnionBig_Performance()
         {
-            _ = union.Int;
+            int i = 0;
+
+            Measure.Method(() => {
+                Profiler.BeginSample("UnionBig");
+                {
+                    var u = new UnionBig(i);
+                    Process(u);
+                    i++;
+                }
+                Profiler.EndSample();
+            })
+                .WarmupCount(1)
+                .IterationsPerMeasurement(5000)
+                .MeasurementCount(20)
+                .Run();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Process(in Union v)
+        {
+            _ = v.Int;
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Process(in UnionBig v)
+        {
+            _ = v.intValue;
+        }
+
+        [StructLayout(LayoutKind.Explicit, Size = 9996)]
+        private struct Big { }
+
+        [StructLayout(LayoutKind.Explicit)]
+        private struct UnionBig
+        {
+            [FieldOffset(0)] public int intValue;
+            [FieldOffset(0)] public Big bigValue;
+
+            public UnionBig(int intValue)
+            {
+                this.intValue = intValue;
+            }
         }
     }
 }
