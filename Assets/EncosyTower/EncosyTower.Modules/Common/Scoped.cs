300f377ddf2716dfe5b2fbff7424104b64b4013b
Laicasaane <laicasaane@outlook.com>
2024-11-13 12:02:43 +0700

    Rename T to Scoped

--- Assets/EncosyTower/EncosyTower.Modules/Common/Scoped.cs
+++ Assets/EncosyTower/EncosyTower.Modules/Common/Scoped.cs
@@ -4,43 +4,43 @@ using System.Runtime.CompilerServices;
 
 namespace EncosyTower.Modules
 {
-    public readonly struct T<TScope, TValue> : IEquatable<T<TScope, TValue>>
+    public readonly struct Scoped<TScope, TValue> : IEquatable<Scoped<TScope, TValue>>
         where TValue : IEquatable<TValue>
     {
         public readonly TValue Value;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public T([NotNull] TValue value)
+        public Scoped([NotNull] TValue value)
         {
             Value = value;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public bool Equals(T<TScope, TValue> other)
+        public bool Equals(Scoped<TScope, TValue> other)
             => Value.Equals(other.Value);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public override bool Equals(object obj)
-            => obj is T<TScope, TValue> other && Value.Equals(other.Value);
+            => obj is Scoped<TScope, TValue> other && Value.Equals(other.Value);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public override int GetHashCode()
             => Value.GetHashCode();
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static implicit operator T<TScope, TValue>([NotNull] TValue value)
+        public static implicit operator Scoped<TScope, TValue>([NotNull] TValue value)
             => new(value);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static implicit operator TValue(T<TScope, TValue> value)
+        public static implicit operator TValue(Scoped<TScope, TValue> value)
             => value.Value;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool operator ==(T<TScope, TValue> left, T<TScope, TValue> right)
+        public static bool operator ==(Scoped<TScope, TValue> left, Scoped<TScope, TValue> right)
             => left.Equals(right);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool operator !=(T<TScope, TValue> left, T<TScope, TValue> right)
+        public static bool operator !=(Scoped<TScope, TValue> left, Scoped<TScope, TValue> right)
             => !left.Equals(right);
     }
 }
