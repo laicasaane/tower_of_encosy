41dae57e1b2b57f220243d8da2dee5768ca1d5af
Laicasaane <laicasaane@outlook.com>
2024-11-27 16:06:36 +0700

    IsExternalInit should be internal

--- Assets/EncosyTower/EncosyTower.Modules/System.Runtime.CompilerServices/IsExternalInit.cs
+++ Assets/EncosyTower/EncosyTower.Modules/System.Runtime.CompilerServices/IsExternalInit.cs
@@ -3,7 +3,7 @@ using UnityEngine.Scripting;
 namespace System.Runtime.CompilerServices
 {
     [Preserve]
-    public sealed class IsExternalInit
+    internal sealed class IsExternalInit
     {
         [Preserve]
         public IsExternalInit() { }
