da6a91266868138ab99b69065998fccd91675ff8
Laicasaane <laicasaane@outlook.com>
2024-11-30 01:06:07 +0700

    Prepare for testing module

---
+++ Assets/EncosyTower/EncosyTower.Modules.Testing/TestRunner.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace EncosyTower.Modules.Testing
+{
+    internal static class TestRunner
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        public static void Run()
+        {
+
+        }
+    }
+}
