74c9e580b089f6e2ce8ab4583dc6ede984757fba
Laicasaane <laicasaane@outlook.com>
2025-03-12 10:09:47 +0700

    Add extension method for collection of IDisposable

---
+++ Assets/EncosyTower/EncosyTower.Core/Collections.Extensions/DisposableCollectionExtensions.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+namespace EncosyTower.Collections
+{
+    public static class DisposableCollectionExtensions
+    {
+        public static void Dispose<TCollection>(this TCollection self)
+            where TCollection : ICollection<IDisposable>
+        {
+            if (self == null)
+            {
+                return;
+            }
+
+            foreach (var disposable in self)
+            {
+                disposable?.Dispose();
+            }
+
+            self.Clear();
+        }
+    }
+}
