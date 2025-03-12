50c36f602ad03b94c2ff13f56ea8ac8f0d691eca
Laicasaane <laicasaane@outlook.com>
2025-03-12 10:17:47 +0700

    Add extension method for RectTransform

--- Assets/EncosyTower/EncosyTower.Core/UnityEngine.Extensions/EncosyComponentExtensions.cs
+++ Assets/EncosyTower/EncosyTower.Core/UnityEngine.Extensions/EncosyComponentExtensions.cs
@@ -25,6 +25,33 @@ namespace EncosyTower.UnityExtensions
             self.SetParent(null, worldPositionStays);
         }
 
+        public static void FillParent([NotNull] this RectTransform self, [NotNull] RectTransform parent)
+        {
+            ThrowIfComponentInvalid(self);
+            ThrowIfComponentInvalid(parent);
+
+            self.SetParent(parent, false);
+            self.localPosition = Vector3.zero;
+            self.anchorMin = Vector2.zero;
+            self.anchorMax = Vector2.one;
+            self.offsetMin = Vector2.zero;
+            self.offsetMax = Vector2.zero;
+            self.pivot = new Vector2(0.5f, 0.5f);
+            self.rotation = Quaternion.identity;
+            self.localScale = Vector3.one;
+        }
+
+        public static void SetParentRect([NotNull] this RectTransform self, [NotNull] RectTransform parent)
+        {
+            ThrowIfComponentInvalid(self);
+            ThrowIfComponentInvalid(parent);
+
+            self.SetParent(parent, false);
+            self.localPosition = Vector3.zero;
+            self.rotation = Quaternion.identity;
+            self.localScale = Vector3.one;
+        }
+
         [HideInCallstack, Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
         private static void ThrowIfComponentInvalid(Component self)
         {
