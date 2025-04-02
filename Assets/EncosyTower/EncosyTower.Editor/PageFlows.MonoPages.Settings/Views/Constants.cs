ac87c5a7d591ef08e75f6cf89b1e71e2232ac2f1
Laicasaane <laicasaane@outlook.com>
2025-04-02 14:01:47 +0700

    Remove unused code

--- Assets/EncosyTower/EncosyTower.Editor/PageFlows.MonoPages.Settings/Views/Constants.cs
+++ Assets/EncosyTower/EncosyTower.Editor/PageFlows.MonoPages.Settings/Views/Constants.cs
@@ -10,8 +10,5 @@ namespace EncosyTower.Editor.PageFlows.MonoPages.Settings.Views
         private const string FILE_NAME = nameof(MonoPageFlowSettings);
 
         public const string THEME_STYLE_SHEET = $"{STYLE_SHEETS_PATH}/{FILE_NAME}.tss";
-        public const string STYLE_SHEET_DARK = $"{STYLE_SHEETS_PATH}/{FILE_NAME}_Dark.uss";
-        public const string STYLE_SHEET_LIGHT = $"{STYLE_SHEETS_PATH}/{FILE_NAME}_Light.uss";
-        public const string STYLE_SHEET_RESOURCES = $"{STYLE_SHEETS_PATH}/{FILE_NAME}_Resources.uxml";
     }
 }
