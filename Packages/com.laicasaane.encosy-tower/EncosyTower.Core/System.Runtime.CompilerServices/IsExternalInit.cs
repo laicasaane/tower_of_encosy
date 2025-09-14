e5ca9369c30c62d90cfc7604a0b43da394bee74e
Laicasaane <laicasaane@outlook.com>
2025-09-15 00:20:59 +0700

    Correct compilation symbol

--- Packages/com.laicasaane.encosy-tower/EncosyTower.Core/System.Runtime.CompilerServices/IsExternalInit.cs
+++ Packages/com.laicasaane.encosy-tower/EncosyTower.Core/System.Runtime.CompilerServices/IsExternalInit.cs
@@ -1,7 +1,14 @@
-#if !DISABLE_ENCOSY_ISEXTERNALINIT
+#if !NET5_0_OR_GREATER
 
 using UnityEngine.Scripting;
 
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+// This is needed due to NativeAOT which doesn't enable nullable globally yet
+#nullable enable
+
+// Allow use of init setters on downlevel frameworks.
 namespace System.Runtime.CompilerServices
 {
     [Preserve]
