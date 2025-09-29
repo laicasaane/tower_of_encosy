c4b0b833864eb2169749f9d2f1457e68442e098b
Laicasaane <laicasaane@outlook.com>
2025-09-29 21:10:16 +0700

    Add tests for ListFast

---
+++ Packages/com.laicasaane.encosy-tower/EncosyTower.Tests.EditorMode/EncosyTower.Core/Collections/ListFastTests.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using EncosyTower.Collections;
+using NUnit.Framework;
+
+namespace EncosyTower.Tests.EncosyTower.Collections
+{
+    public class ListFastTests
+    {
+        [Test]
+        public void ListFast_Tests()
+        {
+            var list = new ListFast<int>(new List<int>(4));
+
+            Assert.AreEqual(true, list.IsValid);
+            Assert.AreEqual(4, list.Capacity);
+            Assert.AreEqual(0, list.Count);
+
+            list.Add(5);
+            list.Add(8);
+            list.Add(2);
+            list.Add(0);
+
+            Assert.AreEqual(4, list.Count);
+            Assert.AreEqual(5, list[0]);
+            Assert.AreEqual(8, list[1]);
+            Assert.AreEqual(2, list[2]);
+            Assert.AreEqual(0, list[3]);
+
+            list.Remove(8);
+            Assert.AreEqual(3, list.Count);
+            Assert.AreEqual(2, list[1]);
+        }
+    }
+}
