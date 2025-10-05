7e9913cdc78be1d453cf9b0e3a7e6def9bb24b52
Laicasaane <laicasaane@outlook.com>
2025-10-05 21:14:49 +0700

    Add extensions for array

---
+++ Packages/com.laicasaane.encosy-tower/EncosyTower.Core/Collections.Extensions/EncosyArrayExtensions.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace EncosyTower.Collections.Extensions
+{
+    public static partial class EncosyArrayExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static List<T> ToList<T>([NotNull] this T[] self)
+            => new(self);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FasterList<T> ToFasterList<T>([NotNull] this T[] self)
+            => new(self);
+    }
+}
