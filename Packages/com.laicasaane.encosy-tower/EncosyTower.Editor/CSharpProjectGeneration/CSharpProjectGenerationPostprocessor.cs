b2555892b77f827c1c160038fd7d9aaefef13259
Laicasaane <laicasaane@outlook.com>
2025-10-09 22:54:54 +0700

    Correctly implement SDK-style C# project generation

---
+++ Packages/com.laicasaane.encosy-tower/EncosyTower.Editor/CSharpProjectGeneration/CSharpProjectGenerationPostprocessor.cs
@@ -0,0 +1,72 @@
+#if UNITY_SDK_STYLE_PROJECTS
+
+using UnityEditor;
+
+namespace EncosyTower.Editor.CSharpProjectGeneration
+{
+    internal sealed class CSharpProjectGenerationPostprocessor : AssetPostprocessor
+    {
+        public static string OnSelectingCSProjectStyle()
+        {
+#if ENABLE_SDK_STYLE_PROJECTS
+            return "SDK";
+#else
+            return "Legacy";
+#endif
+        }
+    }
+
+    internal static class CSharpProjectGenerationMenu
+    {
+        [MenuItem("Encosy Tower/Project Settings/CSharp/Switch to Legacy")]
+        private static void SwitchToLegacy()
+        {
+            UserBuildAPI.RemoveScriptingDefineSymbols(
+                  UserBuildAPI.CurrentBuildTarget
+                , "ENABLE_SDK_STYLE_PROJECTS"
+            );
+
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+        }
+
+        [MenuItem("Encosy Tower/Project Settings/CSharp/Switch to SDK-style")]
+        private static void SwitchToSdkStyle()
+        {
+            UserBuildAPI.RemoveScriptingDefineSymbols(
+                  UserBuildAPI.CurrentBuildTarget
+                , "ENABLE_SDK_STYLE_PROJECTS"
+            );
+
+            UserBuildAPI.AddScriptingDefineSymbols(
+                  UserBuildAPI.CurrentBuildTarget
+                , "ENABLE_SDK_STYLE_PROJECTS"
+            );
+
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+        }
+
+        [MenuItem("Encosy Tower/Project Settings/CSharp/Switch to Legacy", true)]
+        private static bool CanDisableLegacy()
+        {
+#if ENABLE_SDK_STYLE_PROJECTS
+            return true;
+#else
+            return false;
+#endif
+        }
+
+        [MenuItem("Encosy Tower/Project Settings/CSharp/Switch to SDK-style", true)]
+        private static bool CanDisableSdkStyle()
+        {
+#if ENABLE_SDK_STYLE_PROJECTS
+            return false;
+#else
+            return true;
+#endif
+        }
+    }
+}
+
+#endif
