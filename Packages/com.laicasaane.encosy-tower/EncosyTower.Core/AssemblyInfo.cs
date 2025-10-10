8534aef1aca435a8389a41c391f7d221540aa3ff
Laicasaane <laicasaane@outlook.com>
2025-10-10 14:56:34 +0700

    Generate property bag for MonoPageFlowSettings

--- Packages/com.laicasaane.encosy-tower/EncosyTower.Core/AssemblyInfo.cs
+++ Packages/com.laicasaane.encosy-tower/EncosyTower.Core/AssemblyInfo.cs
@@ -1,1 +1,2 @@
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("EncosyTower.Editor")]
+[assembly: Unity.Properties.GeneratePropertyBagsForAssembly]
--- Packages/com.laicasaane.encosy-tower/EncosyTower.Core/PageFlows.MonoPages/Context/MonoPageFlowSettings.cs
+++ Packages/com.laicasaane.encosy-tower/EncosyTower.Core/PageFlows.MonoPages/Context/MonoPageFlowSettings.cs
@@ -1,12 +1,14 @@
 using EncosyTower.Logging;
 using EncosyTower.Pooling;
 using EncosyTower.Settings;
+using Unity.Properties;
 using UnityEngine.Serialization;
 
 namespace EncosyTower.PageFlows.MonoPages
 {
     [Settings(SettingsUsage.RuntimeProject, "Encosy Tower/Mono Page Flow")]
-    public sealed class MonoPageFlowSettings : Settings<MonoPageFlowSettings>
+    [GeneratePropertyBag]
+    public sealed partial class MonoPageFlowSettings : Settings<MonoPageFlowSettings>
     {
         public bool slimPublishingContext = true;
         public bool ignoreEmptySubscriber = true;
