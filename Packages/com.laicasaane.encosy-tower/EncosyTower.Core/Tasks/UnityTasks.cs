84e9d7a25e1c6633a22efb25f2b8904a2df9034b
Laicasaane <laicasaane@outlook.com>
2025-10-18 16:33:43 +0700

    Housekeeping

--- Packages/com.laicasaane.encosy-tower/EncosyTower.Core/Tasks/UnityTasks.cs
+++ Packages/com.laicasaane.encosy-tower/EncosyTower.Core/Tasks/UnityTasks.cs
@@ -1,15 +1,7 @@
 #if UNITASK || UNITY_6000_0_OR_NEWER
 
-#pragma warning disable IDE0005
-
 namespace EncosyTower.Tasks
 {
-#if UNITASK
-    using UnityTask = Cysharp.Threading.Tasks.UniTask;
-#else
-    using UnityTask = UnityEngine.Awaitable;
-#endif
-
     public static partial class UnityTasks
     {
 
