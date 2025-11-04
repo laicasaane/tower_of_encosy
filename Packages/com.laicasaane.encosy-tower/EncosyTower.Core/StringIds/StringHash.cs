d4ecf198738312ae100add90eb3903262ecbfa47
Laicasaane <laicasaane@outlook.com>
2025-11-04 16:24:46 +0700

    Make StringHash burstable

--- Packages/com.laicasaane.encosy-tower/EncosyTower.Core/StringIds/StringHash.cs
+++ Packages/com.laicasaane.encosy-tower/EncosyTower.Core/StringIds/StringHash.cs
@@ -1,23 +1,11 @@
-using System.Runtime.CompilerServices;
 using EncosyTower.Common;
+using EncosyTower.TypeWraps;
 
 namespace EncosyTower.StringIds
 {
     /// <summary>
     /// Represents the hash value of a string using <see cref="HashValue64"/>.
     /// </summary>
-    internal readonly partial record struct StringHash(ulong Value)
-    {
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public override int GetHashCode()
-            => Value.GetHashCode();
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static implicit operator StringHash(ulong value)
-            => new(value);
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static implicit operator ulong(StringHash hash)
-            => hash.Value;
-    }
+    [WrapRecord]
+    internal readonly partial record struct StringHash(ulong Value);
 }
