edd1b139007cf4161d15813d05a0ba9db0ae77d1
Laicasaane <laicasaane@outlook.com>
2025-11-19 19:03:52 +0700

    Update packages

--- Packages/com.laicasaane.encosy-tower/EncosyTower.Core/Entities/FeatureEntities.cs
+++ Packages/com.laicasaane.encosy-tower/EncosyTower.Core/Entities/FeatureEntities.cs
@@ -7,7 +7,7 @@ namespace EncosyTower.Editor.Entities
     [Feature("4. EncosyTower: Entities")]
     [RequiresPackage(PackageRegistry.Unity, "com.unity.entities", "1.4.3")]
     [RequiresPackage(PackageRegistry.Unity, "com.unity.entities.graphics", "1.4.16", isOptional: true)]
-    [RequiresPackage(PackageRegistry.OpenUpm, "com.latios.latiosframework", "0.14.3", isOptional: true)]
+    [RequiresPackage(PackageRegistry.OpenUpm, "com.latios.latiosframework", "0.14.4", isOptional: true)]
     internal readonly struct FeatureEntities { }
 }
 
