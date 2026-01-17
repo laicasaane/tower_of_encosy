a8169a453d3ad4922c5e3bdaa0da86053b8344e0
Laicasaane <laicasaane@outlook.com>
2026-01-18 00:06:22 +0700

    Add more string extensions, remove redundancy

--- Packages/com.laicasaane.encosy-tower/EncosyTower.Core/Common/EncosyStringExtensions.cs
+++ Packages/com.laicasaane.encosy-tower/EncosyTower.Core/Common/EncosyStringExtensions.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Runtime.CompilerServices;
 
 namespace EncosyTower.Common
@@ -15,6 +14,27 @@ namespace EncosyTower.Common
             => string.IsNullOrEmpty(self) == false;
 
         /// <summary>
+        /// Indicates whether the specified string is null or empty.
+        /// </summary>
+        /// <param name="self">The string to check for null or empty.</param>
+        /// <returns><c>true</c> if the string is null or empty; otherwise, <c>false</c>.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsEmpty(this string self)
+            => string.IsNullOrEmpty(self);
+
+        /// <summary>
+        /// Indicates whether the specified string is null or empty or consists only of white-space characters.
+        /// </summary>
+        /// <param name="self">The string to check for null or empty.</param>
+        /// <returns>
+        /// <c>true</c> if the string is null or empty or consists only of white-space characters;
+        /// otherwise, <c>false</c>.
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsEmptyOrWhiteSpace(this string self)
+            => string.IsNullOrWhiteSpace(self);
+
+        /// <summary>
         /// Returns <paramref name="self"/> if it is not null or empty; otherwise, returns the specified default value.
         /// </summary>
         /// <param name="self">The string to check for null or empty.</param>
@@ -25,24 +45,13 @@ namespace EncosyTower.Common
             => string.IsNullOrEmpty(self) ? defaultValue : self;
 
         /// <summary>
-        /// Indicates whether the specified <paramref name="self"/> is empty or consists only of white-space characters.
+        /// Returns <paramref name="self"/> if it is not null or white-space; otherwise, returns the specified default value.
         /// </summary>
-        /// <param name="self">The <c>ReadOnlySpan&lt;char&gt;</c> to check.</param>
-        /// <returns>
-        /// <c>true</c> if <paramref name="self"/> is empty or consists only of white-space characters;
-        /// otherwise, <c>false</c>.
-        /// </returns>
-        public static bool IsEmptyOrWhiteSpace(in this ReadOnlySpan<char> self)
-        {
-            for (int i = 0; i < self.Length; i++)
-            {
-                if (char.IsWhiteSpace(self[i]) == false)
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
+        /// <param name="self">The string to check for null or white-space.</param>
+        /// <param name="defaultValue">The value to return if the original string is null or white-space. Can be null.</param>
+        /// <returns>The original string if it is not null or white-space; otherwise, the specified default value.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string NotEmptyWhiteSpaceOr(this string self, string defaultValue)
+            => string.IsNullOrWhiteSpace(self) ? defaultValue : self;
     }
 }
