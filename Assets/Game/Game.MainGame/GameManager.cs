edda238d6777ad3d618d8c8713f54a62d86f9478
Laicasaane <laicasaane@outlook.com>
2026-01-20 19:33:43 +0700

    Add simple sample for message interceptor

--- Assets/Game/Game.MainGame/GameManager.cs
+++ Assets/Game/Game.MainGame/GameManager.cs
@@ -1,3 +1,4 @@
+using Cysharp.Threading.Tasks;
 using EncosyTower.PubSub;
 using UnityEngine;
 
@@ -12,11 +13,18 @@ namespace Module.MainGame
         private void Awake()
         {
             Debug.Log("[GameManager] Awake");
+            Debug.Log("[GameManager] Add an interceptor...");
+
+            GlobalMessenger.Interceptors.AddInterceptor(new Interceptor());
+
+            Debug.Log("[GameManager] Publishing a Test Message with content 'From Awake'...");
 
             GlobalMessenger.Publisher.UnityScope(this).Publish(
                   new TestMsg("From Awake")
                 , PublishingContext.WaitForSubscriber(token: destroyCancellationToken)
             );
+
+            Debug.Log("[GameManager] Test Message published. Wait for response...");
         }
 
         private void Update()
@@ -33,9 +41,19 @@ namespace Module.MainGame
 
         private static void Handle(GameManager state, TestMsg msg)
         {
-            Debug.Log($"[GameManager] TestMsg received: {msg.Text}");
+            Debug.Log($"[GameManager] Test Message received: {msg.Text}");
         }
 
         private readonly record struct TestMsg(string Text) : IMessage;
+
+        private class Interceptor : IMessageInterceptor<TestMsg>
+        {
+            public UniTask InterceptAsync(TestMsg msg, PublishingContext context, PublishContinuation<TestMsg> continuation)
+            {
+                Debug.Log("[GameManager+Interceptor] Intercepting Test Message, prefixed with 'Intercepted: '.");
+
+                return continuation(msg with { Text = $"Intercepted: {msg.Text}" }, context);
+            }
+        }
     }
 }
