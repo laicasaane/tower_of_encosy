6d77abfb0c5e37d18a0d285354c6ff1a6791287b
Laicasaane <laicasaane@outlook.com>
2026-03-28 02:55:10 +0700

    Update packages

--- Packages/com.laicasaane.encosy-tower/EncosyTower.Core/Localization/FeatureLocalization.cs
+++ Packages/com.laicasaane.encosy-tower/EncosyTower.Core/Localization/FeatureLocalization.cs
@@ -6,7 +6,7 @@ namespace EncosyTower.Editor.Localization
 {
     [Feature("2. EncosyTower: Localization")]
     [RequiresPackage(PackageRegistry.Unity, "com.unity.addressables", "2.9.1")]
-    [RequiresPackage(PackageRegistry.Unity, "com.unity.localization", "1.5.10")]
+    [RequiresPackage(PackageRegistry.Unity, "com.unity.localization", "1.5.11")]
     internal readonly struct FeatureLocalization { }
 }
 
