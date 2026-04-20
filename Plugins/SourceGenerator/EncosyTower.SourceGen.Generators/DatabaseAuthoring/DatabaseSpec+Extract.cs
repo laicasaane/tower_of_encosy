2402d85fb37087c34ab16031f8b36da8dd5a5272
Laicasaane <laicasaane@outlook.com>
2026-04-21 00:04:41 +0700

    SourceGen: house keeping

--- Plugins/SourceGenerator/EncosyTower.SourceGen.Generators/DatabaseAuthoring/DatabaseSpec+Extract.cs
+++ Plugins/SourceGenerator/EncosyTower.SourceGen.Generators/DatabaseAuthoring/DatabaseSpec+Extract.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.IO;
 using System.Threading;
 using EncosyTower.SourceGen.Helpers.Data;
 using Microsoft.CodeAnalysis;
