a854782db6abfee00a33854b598892624c0bef82
Laicasaane <laicasaane@outlook.com>
2026-05-27 17:44:22 +0700

    Conversion: add interfaces IToFixedString and IToDisplayString

--- Packages/com.laicasaane.encosy-tower/EncosyTower.Core/Conversion/IToFixedString.cs
+++ Packages/com.laicasaane.encosy-tower/EncosyTower.Core/Conversion/IToFixedString.cs
@@ -20,6 +20,22 @@ namespace EncosyTower.Conversion
     {
         T ToDisplayFixedString();
     }
+
+    public interface IToFixedString
+    {
+        T ToFixedString<T>()
+            where T : unmanaged, INativeList<byte>, IIndexable<byte>, IUTF8Bytes
+                    , IComparable<string>, IEquatable<string>
+                    , IComparable<T>, IEquatable<T>;
+    }
+
+    public interface IToDisplayFixedString
+    {
+        T ToDisplayFixedString<T>()
+            where T : unmanaged, INativeList<byte>, IIndexable<byte>, IUTF8Bytes
+                    , IComparable<string>, IEquatable<string>
+                    , IComparable<T>, IEquatable<T>;
+    }
 }
 
 #endif
