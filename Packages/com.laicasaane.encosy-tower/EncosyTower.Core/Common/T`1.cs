e5e597b8cbd48f1ab6edbb8b9568b7c771807756
Laicasaane <laicasaane@outlook.com>
2026-05-31 03:24:18 +0700

    Core: remove exception for nesting T<>, nesting is allowed

--- Packages/com.laicasaane.encosy-tower/EncosyTower.Core/Common/T`1.cs
+++ Packages/com.laicasaane.encosy-tower/EncosyTower.Core/Common/T`1.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Diagnostics;
 using System.Runtime.CompilerServices;
-using EncosyTower.Types;
-using UnityEngine;
 
 namespace EncosyTower.Common
 {
@@ -11,7 +7,6 @@ namespace EncosyTower.Common
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T<TType> T<TType>()
         {
-            ThrowIfUnsupported<TType>();
             return new();
         }
 
@@ -23,8 +18,6 @@ namespace EncosyTower.Common
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Option<TDest> TryCastTo<TSource, TDest>(this TSource source, T<TDest> _)
         {
-            ThrowIfUnsupported<TDest>();
-
             if (source is TDest dest)
             {
                 return Option.Some(dest);
@@ -41,25 +34,7 @@ namespace EncosyTower.Common
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Option<TDest> TryCastFrom<TSource, TDest>(this T<TDest> _, TSource source)
             => TryCastTo(source, _);
-
-        [MethodImpl(MethodImplOptions.NoInlining), HideInCallstack, StackTraceHidden]
-        internal static void ThrowIfUnsupported<T>()
-        {
-            if (typeof(T).IsGenericType && typeof(T).GetGenericTypeDefinition() == typeof(T<>))
-            {
-                throw new NotSupportedException(
-                    $"Nested T<> is not supported." +
-                    $"Type 'T<{typeof(T).GetFriendlyName()}>' cannot be wrapped in another T<>."
-                );
-            }
-        }
     }
 
-    public readonly struct T<TType>
-    {
-        public T()
-        {
-            GenericT.ThrowIfUnsupported<TType>();
-        }
-    }
+    public readonly struct T<TType> { }
 }
