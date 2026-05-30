c41f7c36f998cb215bc6d09ae534f3202cc448e0
Laicasaane <laicasaane@outlook.com>
2026-05-30 20:32:05 +0700

    Data.Authoring: rename attribute DataManualAuthoringAttribute

---
+++ Packages/com.laicasaane.encosy-tower/EncosyTower.Core/Data.Authoring/DataManualAuthoringAttribute.cs
@@ -0,0 +1,38 @@
+using System;
+using EncosyTower.Core;
+
+namespace EncosyTower.Data.Authoring
+{
+    /// <summary>
+    /// <para>
+    /// Informs the authoring pipeline that the annotated property or field should be assigned by
+    /// a manual authoring mechanism which hooks into callbacks provided by
+    /// <see cref="EncosyTower.Databases.Authoring.IDataSheet"/>.
+    /// </para>
+    /// <para>
+    /// To hook into <c>IDataSheet</c> callbacks, implement these partial methods on the authoring sheet
+    /// derived from <see cref="Cathei.BakingSheet.SheetRow{TKey}"/>:
+    /// <list type="bullet">
+    /// <item>OnPreprocess</item>
+    /// <item>OnProcess</item>
+    /// <item>OnPostprocess</item>
+    /// </list>
+    /// (Note: It is not required to implement all of them, it's best to choose only methods that make sense.)
+    /// </para>
+    /// </summary>
+    /// <remarks>
+    /// The generated property will be annotated with <see cref="Cathei.BakingSheet.NonSerializedAttribute"/>,
+    /// and its name will be suffixed with <c>_Manual</c>.
+    /// </remarks>
+    [ApiForAuthoring]
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public sealed class DataManualAuthoringAttribute : Attribute
+    {
+        public DataManualAuthoringAttribute(bool emiRawStringProperty = true)
+        {
+            EmitRawStringProperty = emiRawStringProperty;
+        }
+
+        public bool EmitRawStringProperty { get; }
+    }
+}
