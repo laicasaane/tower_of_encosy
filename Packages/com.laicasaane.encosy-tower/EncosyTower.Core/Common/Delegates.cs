4d6a3d3a2f9b3300eb6d487b1171b3b34c648497
Laicasaane <laicasaane@outlook.com>
2026-06-26 16:19:45 +0700

    Core: add interfaces for some delegate types

--- Packages/com.laicasaane.encosy-tower/EncosyTower.Core/Common/Delegates.cs
+++ Packages/com.laicasaane.encosy-tower/EncosyTower.Core/Common/Delegates.cs
@@ -13,4 +13,29 @@ namespace EncosyTower.Common
     public delegate bool PredicateIn<T>(in T arg);
 
     public delegate bool PredicateRef<T>(ref T arg);
+
+    public interface IFunc<TResult>
+    {
+        TResult Invoke();
+    }
+
+    public interface IFuncRef<T, TResult>
+    {
+        TResult Invoke(ref T p);
+    }
+
+    public interface IActionRef<T>
+    {
+        void Invoke(ref T p);
+    }
+
+    public interface IActionRef<T1, T2>
+    {
+        void Invoke(ref T1 p1, ref T2 p2);
+    }
+
+    public interface IPredicateRef<T>
+    {
+        bool Invoke(ref T p);
+    }
 }
